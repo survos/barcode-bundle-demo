22445ee62213b3cab193b6aa291d96938db47310
Tac Tacelosky <tacman@gmail.com>
2022-10-04 16:31:39 -0400

    add qr reader wrapper, requires python

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,16 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
-    public function index(): Response
+    public function index(
+        #[Autowire('%kernel.project_dir%/public')]
+        string $imageDir,
+    ): Response
     {
+
+        $imagePath = $imageDir . '/2-qr-codes.jpg';
+        assert(file_exists($imagePath), $imagePath . ' not found');
+        $zbar = new \TarfinLabs\ZbarPhp\Zbar($imagePath);
+        $code = $zbar->scan();
+        dd($code);
+
+
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
         ]);
     }
+
 }
